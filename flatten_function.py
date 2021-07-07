7960191024745959e6c131a39578ab5bc4152c30
Mua12 <83073342+Mua12@users.noreply.github.com>
2021-07-07 16:18:48 +0300

    Update flatten_function.py

--- flatten_function.py
+++ flatten_function.py
@@ -7,17 +7,17 @@ non-scalar verilerden de oluşabilir.
 input : [[1, 'a', ['cat'], 2], [[[3]], 'dog'], 4, 5]
 output: [ 1, 'a', 'cat', 2, 3, 'dog', 4, 5 ]
 """""
-sample_list = [ [1, 'a', ['cat'], 2], [ [[3]], 'dog'], 4, 5]
+sample_list = [ [1, 'a', ['cat'], 2], [ [[3]], 'dog'], 4, 5] 
 
-def flatten_list(i_list):
-    def _flatten_list(i_list, r):    
-        if type(i_list) is not list:
-            r.append(i_list)
+def flatten_list(L):
+    def _flatten_list(L, result):    
+        if type(L) is not list and type(L) is not tuple:
+            result.append(L)
         else:
-            for e in i_list:
-                r += flatten_list(e)
-        return r
-    return _flatten_list(i_list, [])       
+            for e in L:
+                result += flatten_list(e)
+        return result
+    return _flatten_list(L, [])       
 
 print(f"Flattened Sample List: {flatten_list( sample_list )} ") 
 # output ----------> [ 1, 'a', 'cat', 2, 3, 'dog', 4, 5]
